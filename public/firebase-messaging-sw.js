5b9479c39917abddef55dd9ddeedf95ccb80aadf
Krushna Chandra Rout <Krushna.Chandra@visbbsr.com>
2022-12-26 13:04:33 +0530

    change cred for expr acc

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,13 +7,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compa
 
 // Initialize the Firebase app in the service worker by passing the generated config
 var firebaseConfig = {
-    apiKey: "AIzaSyAN_p-yfj7PP_-cqFfIga0E38xaXwwZJNY",
-    authDomain: "chatapp-by-kc.firebaseapp.com",
-    projectId: "chatapp-by-kc",
-    storageBucket: "chatapp-by-kc.appspot.com",
-    messagingSenderId: "295571164845",
-    appId: "1:295571164845:web:52b31e0860e5fbd891c024",
-    measurementId: "G-YC3SXX3XS8"
+    apiKey: "AIzaSyCo_mAoSpnhpDKCf-sN8TAy54Bem9lg93E",
+    authDomain: "chatapp-by-kcr.firebaseapp.com",
+    projectId: "chatapp-by-kcr",
+    storageBucket: "chatapp-by-kcr.appspot.com",
+    messagingSenderId: "618273233205",
+    appId: "1:618273233205:web:482566996fa9a19eb3d47c",
+    measurementId: "G-KYH4G90Z8G"
 };
 
 firebase.initializeApp(firebaseConfig);
